4a96463274e45b1b9ba1876a41ec0ca9055061ab
felix <54502652+TheVideoFelix@users.noreply.github.com>
2024-12-30 12:45:43 +0100

    feat(layout): Implemented a var for setting an floatingActionButton.

--- lib/layout/base.dart
+++ lib/layout/base.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class BaseLayout extends StatelessWidget {
+  final Widget? actionButton;
   final Widget? header;
   final Widget body;
   final Widget? footer;
 
-
   const BaseLayout({
+    this.actionButton,
     this.header,
     required this.body,
     this.footer,
@@ -16,10 +17,12 @@ class BaseLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: header is PreferredSizeWidget ? header as PreferredSizeWidget : null,
+      floatingActionButton: actionButton,
+      appBar:
+          header is PreferredSizeWidget ? header as PreferredSizeWidget : null,
       body: Column(
         children: [
-          if (header != null &&  header is! PreferredSizeWidget) header!,
+          if (header != null && header is! PreferredSizeWidget) header!,
           Expanded(child: body),
           if (footer != null) footer!,
         ],
