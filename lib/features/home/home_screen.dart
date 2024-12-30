5a9c29b1ce4e1cc4434c5ad14ece6f3dcc2d0fd0
felix <54502652+TheVideoFelix@users.noreply.github.com>
2024-12-30 12:59:59 +0100

    feat(features): Change the layout of home screen.

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:plan_your_live/layout/base.dart';
+import 'package:plan_your_live/providers/navigation.dart';
+import 'package:plan_your_live/shared/widgets/card.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    bool test = true;
     return BaseLayout(
       header: AppBar(title: const Text('Home Screen')),
-      body: Column(
-        children: [
-          const Text('Hello World'),
-          ElevatedButton(onPressed: () {
-            Navigator.pushNamed(context, '/todolists');
-          }, child: const Text('Go to Todolists'))
-        ],
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              child: const Text('Welcome back', style: TextStyle(
+                fontSize: 30
+              )),
+            ),
+          ],
+        ),
       ),
     );
   }
