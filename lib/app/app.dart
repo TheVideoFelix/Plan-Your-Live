15a539928be7a4874c61cde77f939f59878c5071
felix <54502652+TheVideoFelix@users.noreply.github.com>
2025-03-19 22:24:46 +0100

    feat(app): added global fetching of data in app.dart.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,12 +3,26 @@ import 'package:plan_your_live/features/home/home_screen.dart';
 import 'package:plan_your_live/features/todolist/todolist_screen.dart';
 import 'package:plan_your_live/features/todolists/todolsits_screen.dart';
 import 'package:plan_your_live/providers/navigation.dart';
+import 'package:plan_your_live/providers/todolist.dart';
 import 'package:plan_your_live/shared/utils/const.dart';
 import 'package:plan_your_live/shared/widgets/navigation/bottom_navigation.dart';
 import 'package:provider/provider.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<void> _fetchDataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDataFuture = Provider.of<TodolistNotifier>(context, listen: false).fetchAndSetTodolistWithTodos();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,16 +37,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: Constants.theme,
       title: Constants.appName,
-      home: Scaffold(
-        body: PageView(
-          controller: navigationProvider.pageController,
-          onPageChanged: (int index) {
-            navigationProvider.setIndex(index);
-          },
-          children: List.generate(3, (index) => pages[index]),
-        ),
-        bottomNavigationBar: const BottomNavigation(),
-      ),
+      home: FutureBuilder(
+          future:_fetchDataFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                body: Center(child: CircularProgressIndicator()
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return const Scaffold(body: Center(child: Text('An error occurred!')));
+            } else {
+              return Scaffold(
+                body: PageView(
+                  controller: navigationProvider.pageController,
+                  onPageChanged: (int index) {
+                    navigationProvider.setIndex(index);
+                  },
+                  children: List.generate(3, (index) => pages[index]),
+                ),
+                bottomNavigationBar: const BottomNavigation(),
+              );
+            }
+          }),
     );
   }
 }
