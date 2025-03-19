05302d1238a4bf69b4582b7c9badaafcd632fbf8
felix <54502652+TheVideoFelix@users.noreply.github.com>
2025-03-19 20:42:06 +0100

    feat: Initialize Flutter bindings and await database initialization in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:plan_your_live/app/app.dart';
 import 'package:plan_your_live/app/providers.dart';
+import 'package:plan_your_live/shared/db/database_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseService().database;
   runApp(const AppProvider(child: MyApp()));
 }
